f048362b66cefad1eb07da09cdb19af888ef611f
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-11-21 20:05:52 +0300

    upd

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['@/assets/css/roboto.css'],
+  publicRuntimeConfig: { apiToken: process.env.STORYBLOK_API_KEY },
   modules: [
-    ['@storyblok/nuxt', { accessToken: 'bV2WMUZdHsgfuWrloRoUggtt' }],
+    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_API_KEY }],
     '@nuxtjs/tailwindcss',
   ],
 })
